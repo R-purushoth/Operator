11863f90262301da778d4938809d54ed675bccad
shothp345@gmail.com <shothp345@gmail.com>
2025-07-25 14:58:57 +0530

    first commit

---
+++ src/ArithmeticOpt.java
@@ -0,0 +1,7 @@
+public class ArithmeticOpt {
+    public static void main(String[] args) {
+        // Arithmetic operator to perform mathematical operation link Add(+), sub(-), mul(*), div(/) and mod(%). etc..
+        int a=23, b=3; // input value
+        System.out.println("Addition of A and B is :"+ (a+b));
+    }
+}
